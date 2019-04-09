6e96af7a58d3b00045d9a0004ede4907c07feee2
jeffnwarner <jnwarner@ucdavis.edu>
2019-04-09 14:54:14 -0700

    replaces multiple words now

--- content.js
+++ content.js
@@ -1,4 +1,6 @@
 var elements = document.getElementsByTagName('*');
+var swears = ["fuck", "shit", "ass", "cunt"];
+var replacers = ["fudge", "poop", "butt", "crab"];
 
 for (var i = 0; i < elements.length; i++) {
     var element = elements[i];
@@ -8,8 +10,10 @@ for (var i = 0; i < elements.length; i++) {
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-            var replacedText = text.replace(/fuck/gi, 'fudge');
-
+			var replacedText = text;
+			for (var k = 0; k < swears.length; k++) {
+				var replacedText = replacedText.replace(new RegExp(swears[k], "gi"), replacers[k]);
+			}
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
             }
